197740fdf1430594e72c504a542c3a12e4c83dea
Mike Hovedskov <deengo113@gmail.com>
2024-04-30 22:42:39 +0200

    added static fileserver to backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+var path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
